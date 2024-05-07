98cb89e43d5a40658410ecd7b5d9df42b67a778e
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-07 19:07:55 +0900

    feat: Placeholderの下に表示される並列した文字を描画

--- lib/main_page_layout.dart
+++ lib/main_page_layout.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // 大枠のウィジェット
@@ -35,9 +36,82 @@ class CenterParts extends StatelessWidget {
             aspectRatio: 1,
             child: Placeholder(),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TemperatureText(
+                textColor: Colors.red,
+              ),
+              TemperatureText(
+                textColor: Colors.blue,
+              ),
+            ],
+          ),
         ],
       ),
     );
+  }
+}
+
+// 温度の表示用ウィジェット
+class TemperatureText extends StatelessWidget {
+  const TemperatureText({
+    required this.textColor,
+    super.key,
+  });
+  final Color textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: SizedBox(
+          width: 50,
+          child: Center(
+            child: FormattedText(
+              textWord: '** ℃',
+              textTheme: Theme.of(context)
+                  .textTheme
+                  .labelLarge
+                  ?.copyWith(color: textColor),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(ColorProperty('textColor', textColor));
+  }
+}
+
+// 文字の表示用ウィジェット
+class FormattedText extends StatelessWidget {
+  const FormattedText({
+    required this.textWord,
+    required this.textTheme,
+    super.key,
+  });
+  final String textWord;
+  final TextStyle? textTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      textWord,
+      style: textTheme,
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('textWord', textWord));
+    properties.add(DiagnosticsProperty<TextStyle?>('textTheme', textTheme));
   }
 }
 
