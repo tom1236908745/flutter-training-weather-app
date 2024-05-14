5532cfa44455c44707c30d43689a7591a1f4a784
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-14 15:29:59 +0900

    feat: 初期ページをStatefulWidgetを継承させた

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/main_page_layout.dart';
 
-class StartScreen extends StatelessWidget {
+class StartScreen extends StatefulWidget {
   const StartScreen({super.key});
 
   @override
+  State<StartScreen> createState() => StartScreenState();
+}
+
+class StartScreenState extends State<StartScreen> {
+  @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: const Text('メイン画面へ'),
-      onPressed: () async {
-        final navigatorState = Navigator.of(context);
-        final route = MaterialPageRoute<void>(
-          builder: (context) => const MainPageLayout(),
-        );
-        await navigatorState.push(route);
-      },
+    return ColoredBox(
+      color: Colors.green,
+      child: TextButton(
+        child: const Text('メイン画面へ'),
+        onPressed: () async {
+          final navigatorState = Navigator.of(context);
+          final route = MaterialPageRoute<void>(
+            builder: (context) => const MainPageLayout(),
+          );
+          await navigatorState.push(route);
+        },
+      ),
     );
   }
 }
