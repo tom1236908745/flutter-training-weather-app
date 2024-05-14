7af9965c3a7f4778d89e95ae03b5a3c1e780346b
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-14 15:30:50 +0900

    feat: 既存ページからCloseボタンで初期画面に遷移できる様実装

--- lib/main_page_layout.dart
+++ lib/main_page_layout.dart
@@ -18,7 +18,9 @@ class MainPageLayoutState extends State<MainPageLayout> {
 
   void _updateWeatherCondition(WeatherCondition newWeatherCondition) {
     setState(() {
-      _weatherCondition = newWeatherCondition;
+      if (mounted) {
+        _weatherCondition = newWeatherCondition;
+      }
     });
   }
 
@@ -135,7 +137,9 @@ class _TextButtons extends StatelessWidget {
       children: <Widget>[
         Expanded(
           child: TextButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
             child: const Text('Close'),
           ),
         ),
