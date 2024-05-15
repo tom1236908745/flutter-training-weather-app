e8410a6406524501a213544b26e1ae32fb6965a0
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-15 19:23:21 +0900

    fix: `singleWhereOrNull`を使用した実装に修正

--- lib/constant/weather_condition.dart
+++ lib/constant/weather_condition.dart
@@ -1,11 +1,20 @@
+import 'package:collection/collection.dart';
+
 /// 天気の種類を扱うEnum
 enum WeatherCondition {
   cloudy,
   sunny,
   rainy;
 
-  factory WeatherCondition.from(String conditionName) =>
-      WeatherCondition.values.singleWhere(
-        (element) => element.name == conditionName,
-      );
+  factory WeatherCondition.from(String conditionName) {
+    final result = WeatherCondition.values.singleWhereOrNull(
+      (element) => element.name == conditionName,
+    );
+
+    if (result == null) {
+      throw FormatException('`$conditionName` is invalid format');
+    }
+
+    return result;
+  }
 }
