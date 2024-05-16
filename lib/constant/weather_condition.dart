15dcbd2e47d9d94befedf25f17fb73b2883fd38a
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-16 14:25:42 +0900

    fix: 引数の型をnon-nullに修正

--- lib/constant/weather_condition.dart
+++ lib/constant/weather_condition.dart
@@ -4,7 +4,7 @@ enum WeatherCondition {
   sunny,
   rainy;
 
-  factory WeatherCondition.from(String? conditionName) {
+  factory WeatherCondition.from(String conditionName) {
     return WeatherCondition.values.singleWhere(
       (element) => element.name == conditionName,
       orElse: () => throw FormatException('`$conditionName` is invalid format'),
