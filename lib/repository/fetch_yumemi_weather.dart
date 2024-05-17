34d723a5e66b579f19e1b64da666988db48f93d2
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-17 18:38:05 +0900

    fix: Exceptionを捕捉した際にもダイアログを追加

--- lib/repository/fetch_yumemi_weather.dart
+++ lib/repository/fetch_yumemi_weather.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_training/components/Dialog/error_message_dialog.dart';
+import 'package:flutter_training/constant/set_error_phrase.dart';
 import 'package:flutter_training/constant/weather_condition.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 Future<WeatherCondition?> fetchYumemiWeather(BuildContext context) async {
   final yumemiWeather = YumemiWeather();
-
   try {
     final weatherConditionName = yumemiWeather.fetchThrowsWeather('tokyo');
 
     return WeatherCondition.from(weatherConditionName);
   } on Exception catch (exception) {
     if (kDebugMode) {
-      debugPrint('$exception');
+      await showErrorDialog(
+        context,
+        '$setErrorPhrase$exception',
+      );
     }
   } on YumemiWeatherError catch (error) {
-    await showErrorDialog(context, 'API取得に失敗しました。\n\nエラー内容: $error');
+    await showErrorDialog(
+      context,
+      '$setErrorPhrase$error',
+    );
   }
   return null;
 }
