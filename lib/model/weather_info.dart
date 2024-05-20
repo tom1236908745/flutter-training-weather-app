3c42579e7d25fee8a32fb1250ca6d9d982994ff7
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-20 19:28:47 +0900

    refactor: リファクタ

--- lib/model/weather_info.dart
+++ lib/model/weather_info.dart
@@ -3,6 +3,7 @@ import 'package:flutter_training/constant/weather_condition.dart';
 // API・YumemiWeather から取得するデータ用のモデル層
 class WeatherInfoModel {
   WeatherInfoModel({required this.weatherCondition});
+
   factory WeatherInfoModel.fromJson(String weatherCondition) {
     return WeatherInfoModel(
       weatherCondition: WeatherCondition.from(weatherCondition),
