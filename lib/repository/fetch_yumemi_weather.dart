76c7909330d6880ffec0b952abf09239ff4f0711
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-20 16:21:58 +0900

    fix: debugModeの時以外でもダイアログを表示する様修正

--- lib/repository/fetch_yumemi_weather.dart
+++ lib/repository/fetch_yumemi_weather.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_training/components/Dialog/error_message_dialog.dart';
 import 'package:flutter_training/constant/set_error_phrase.dart';
@@ -12,12 +11,10 @@ Future<WeatherCondition?> fetchYumemiWeather(BuildContext context) async {
 
     return WeatherCondition.from(weatherConditionName);
   } on Exception catch (exception) {
-    if (kDebugMode) {
-      await showErrorDialog(
-        context,
-        '$setErrorPhrase$exception',
-      );
-    }
+    await showErrorDialog(
+      context,
+      '$setErrorPhrase$exception',
+    );
   } on YumemiWeatherError catch (error) {
     await showErrorDialog(
       context,
