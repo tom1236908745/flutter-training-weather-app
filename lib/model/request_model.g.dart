620c29a8255900fc5e79c4ddfdaec1bb8c8946e5
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-22 19:08:42 +0900

    fix: エラー解消(build.yaml内のコードを一部修正)

--- lib/model/request_model.g.dart
+++ lib/model/request_model.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: type=lint, duplicate_ignore
+// ignore_for_file: type=lint, unused_element, duplicate_ignore
 
 part of 'request_model.dart';
 
@@ -8,9 +8,17 @@ part of 'request_model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-RequestModel _$RequestModelFromJson(Map<String, dynamic> json) => RequestModel(
-      area: json['area'] as String,
-      date: json['date'] as String,
+RequestModel _$RequestModelFromJson(Map<String, dynamic> json) =>
+    $checkedCreate(
+      'RequestModel',
+      json,
+      ($checkedConvert) {
+        final val = RequestModel(
+          area: $checkedConvert('area', (v) => v as String),
+          date: $checkedConvert('date', (v) => v as String),
+        );
+        return val;
+      },
     );
 
 Map<String, dynamic> _$RequestModelToJson(RequestModel instance) =>
