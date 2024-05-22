5a0a84477538ef077e0b4934fa6181449782eb15
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-05-22 18:58:16 +0900

    feat: build.yamlの設定を追加

---
+++ lib/model/weather_info_model.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_info_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WeatherInfoModel _$WeatherInfoModelFromJson(Map<String, dynamic> json) =>
+    WeatherInfoModel(
+      weatherCondition:
+          $enumDecode(_$WeatherConditionEnumMap, json['weather_condition']),
+      maxTemperature: (json['max_temperature'] as num).toInt(),
+      minTemperature: (json['min_temperature'] as num).toInt(),
+      date: DateTime.parse(json['date'] as String),
+    );
+
+Map<String, dynamic> _$WeatherInfoModelToJson(WeatherInfoModel instance) =>
+    <String, dynamic>{
+      'weather_condition':
+          _$WeatherConditionEnumMap[instance.weatherCondition]!,
+      'max_temperature': instance.maxTemperature,
+      'min_temperature': instance.minTemperature,
+      'date': instance.date.toIso8601String(),
+    };
+
+const _$WeatherConditionEnumMap = {
+  WeatherCondition.cloudy: 'cloudy',
+  WeatherCondition.sunny: 'sunny',
+  WeatherCondition.rainy: 'rainy',
+};
